0702572d8d4d4102b05786c9a088e96576da3c9f
VYOM PATEL <124497900+Vyom1912@users.noreply.github.com>
2023-12-21 22:51:16 +0530

    Add files via upload
    all file added

---
+++ script.js
@@ -0,0 +1,52 @@
+// API link => GET https://icanhazdadjoke.com/
+
+const jokes = document.querySelector("#joke");
+const jokeBtn = document.querySelector("#jokebtn");
+// ---------------------------------------------------
+// there are 2 method to use api
+// 1.Ajax call using XMLHttpRequest
+// 2.fetch api
+//   i.PROMISE
+//   ii.async-await
+
+// ---------------------------------------------------
+//  (i) using PROMISE
+// ---------------------------------------------------
+
+// const generateJokes = () => {
+//   const setHeader = {
+//     headers: {
+//       Accept: "application/json",
+//     },
+//   };
+
+//   fetch("https://icanhazdadjoke.com", setHeader)
+//     .then((res) => res.json())
+//     .then((data) => (jokes.innerHTML = data.joke))
+//     .catch((error) => console.log(error));
+// };
+
+// ---------------------------------------------------
+//  (ii) async await
+// ---------------------------------------------------
+
+const generateJokes = async () => {
+  try {
+    const setHeader = {
+      headers: {
+        Accept: "application/json",
+      },
+    };
+
+    const res = await fetch("https://icanhazdadjoke.com", setHeader);
+    const data = await res.json();
+    jokes.innerHTML = data.joke;
+  } catch (error) {
+    console.log(`the error is ${error}`);
+  }
+};
+
+// ---------------------------------------------------
+
+jokeBtn.addEventListener("click", generateJokes);
+generateJokes();
